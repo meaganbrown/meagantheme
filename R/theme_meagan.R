e54145e943c2f6ed3b385c94ee2831256f0ca06f
Meagan Brown <meaganbrown15@gmail.com>
2022-08-18 15:26:54 -0700

    Initial commit

---
+++ R/theme_meagan.R
@@ -0,0 +1,18 @@
+#' An awesome theme example
+#'
+#' @return
+#' @export
+#'
+#' @examples
+
+
+theme_meagan <- function() {
+  theme(
+    panel.background = element_rect(fill = "yellow"),
+    panel.grid.major.x = element_line(colour = "purple", linetype = 3, size = 0.5),
+    panel.grid.minor.x = element_blank(),
+    panel.grid.major.y =  element_line(colour = "cyan4", linetype = 3, size = 0.5),
+    axis.text = element_text(colour = "red"),
+    axis.title = element_text(colour = "orange")
+  )
+}

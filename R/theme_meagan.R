77c2e74adae7d2cd5363034d7da9c72c36450856
Meagan Brown <meaganbrown15@gmail.com>
2022-08-18 15:28:09 -0700

    push

--- R/theme_meagan.R
+++ R/theme_meagan.R
@@ -8,11 +8,11 @@
 
 theme_meagan <- function() {
   theme(
-    panel.background = element_rect(fill = "yellow"),
-    panel.grid.major.x = element_line(colour = "purple", linetype = 3, size = 0.5),
+    panel.background = element_rect(fill = "white"),
+    panel.grid.major.x = element_line(colour = "lightgrey", linetype = 3, size = 0.5),
     panel.grid.minor.x = element_blank(),
-    panel.grid.major.y =  element_line(colour = "cyan4", linetype = 3, size = 0.5),
-    axis.text = element_text(colour = "red"),
-    axis.title = element_text(colour = "orange")
+    panel.grid.major.y =  element_line(colour = "lightgrey", linetype = 3, size = 0.5),
+    axis.text = element_text(colour = "black"),
+    axis.title = element_text(colour = "black")
   )
 }
